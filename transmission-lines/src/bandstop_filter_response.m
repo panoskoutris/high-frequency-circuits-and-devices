7f97f55da05d738a172825da95c689f1e3529efd
Panagiotis Koutris <pkoutris@ece.auth.gr>
2025-11-17 01:06:46 +0200

    Implement bandstop filter response calculations
    Removed old documentation and added bandstop filter response calculations and plots.

--- transmission-lines/src/bandstop_filter_response.m
+++ transmission-lines/src/bandstop_filter_response.m
@@ -1,43 +1,80 @@
-# TinyShell — Phase 1
+% Initialization of variables
+fo = 10^9;
+N = 201;
+Zo1 = 101.6;
+Zo2 = 98.45;    Yo2 = 1/Zo2;
+Zo3 = 43.6;     Yo3 = 1/Zo3;
+Zo4 = 50;
+ZL = 50;
 
-TinyShell is a small Unix-style shell written in C for Linux. It implements a basic command loop using POSIX calls (fork(), execve(), waitpid()). The shell shows a prompt, reads a line, splits it into arguments, searches PATH for the executable, runs it in a child process, and the parent waits and prints the exit code. It supports common commands (ls, cat, echo) and exits on `exit` or Ctrl+D.
+% f vector
+f = 0:(3*fo/N):(3*fo); 
 
-## Build
+% β(f)*l
+bl = pi*f/(4*fo);
 
-1. Ensure gcc and make are installed. On Ubuntu:
-```sh
-sudo apt update
-sudo apt install build-essential
-```
+% Calculation of Reflection Coefficient Magnitude and SWR
+Zin3 = Zo4;
+Yin3 = 1./Zin3;
 
-2. In the folder with `tinyshell.c` and the `Makefile`:
-```sh
-make
-```
-This produces an executable named `tinyshell`.
+Yopen3 = 1i*Yo2*tan(bl);
 
-## Run
-```sh
-./tinyshell
-```
+YL2 = Yin3 + Yopen3;
+ZL2 = 1./YL2;
 
-## Example commands
-```sh
-ls -l
-echo Hello
-cat tinyshell.c
-exit
-```
+Zin2 = Zo1*(ZL2 + 1i*Zo1*tan(bl))./(Zo1 + 1i*ZL2.*tan(bl));
+Yin2 = 1./Zin2;
 
-## Clean
-```sh
-make clean
-```
+Yopen2 = 1i*Yo3*tan(bl);
 
-## Files included
-- `tinyshell.c` — source code
-- `Makefile` — build rules
-- `README.txt` — this file
+YL1 = Yin2 + Yopen2;
+ZL1 = 1./YL1;
+
+Zin1 = Zo1*(ZL1 + 1i*Zo1*tan(bl))./(Zo1 + 1i*ZL1.*tan(bl));
+Yin1 = 1./Zin1;
+
+Yopen1 = Yopen3;
+
+Yin = Yin1 +Yopen1;
+Zin = 1./Yin;
+
+% Finally
+S11 = abs((Zin - Zo4)./(Zin + Zo4));
+S11_dB = 20*log(S11);
+SWR = (1 + S11)./(1 - S11);
+
+% Plots
+S11_dB(S11_dB < -60) = -60;
+
+figure
+plot(f,S11_dB);
+title('Reflection Coefficient(dB)')
+hold on
+xlabel("Frequency");
+ylabel("|Γ|(dB)","Rotation",0);
+hold off
+
+SWR(SWR > 10) = 10;
+
+figure
+plot(f,SWR);
+title("SWR")
+hold on
+xlabel("Frequency");
+ylabel("SWR","Rotation",0);
+hold off
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
